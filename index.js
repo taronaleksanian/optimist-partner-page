3e436514edc1e39d1373e5115f0e841b059342a5
Taron <taron.aleksanyan.20@gmail.com>
2023-11-20 23:42:08 +0400

    feat: test deploy

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "./public")));
 
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public", "./index.html"))
+);
 
 app.listen(3000, () => console.log("Listening on 3000"));
