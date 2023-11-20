993254b092f40a637a8074accc0e28721676dd99
Taron <taron.aleksanyan.20@gmail.com>
2023-11-20 22:44:35 +0400

    first-commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
+
+app.listen(3000, () => console.log("Listening on 3000"));
